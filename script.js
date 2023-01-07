0f096411c720ffb8ad1136455d20c5b6769354cc
wilson0205 <wilson0205@gmail.com>
2023-01-07 20:53:19 +0900

    Update script.js
    The previous fix didn’t work

--- script.js
+++ script.js
@@ -1,152 +1,89 @@
 const output = document.getElementById("output");
-let currentNumber = "0";
-let storedNumber = "0";
-let operation = null;
 
-const updateOutput = () => {
+let currentNumber = "";
+let storedNumber = "";
+let operation = "";
+
+const clear = () => {
+  currentNumber = "";
+  storedNumber = "";
+  operation = "";
+  output.textContent = "0";
+};
+
+const negative = () => {
+  currentNumber = currentNumber.charAt(0) === "-" ? currentNumber.substr(1) : `-${currentNumber}`;
   output.textContent = currentNumber;
 };
 
-const clear = () => {
-  currentNumber = "0";
-  storedNumber = "0";
-  operation = null;
-  updateOutput();
+const percent = () => {
+  currentNumber = String(parseFloat(currentNumber) / 100);
+  output.textContent = currentNumber;
+};
+
+const performOperation = () => {
+  storedNumber = parseFloat(storedNumber);
+  currentNumber = parseFloat(currentNumber);
+  if (operation === "+") {
+    currentNumber = storedNumber + currentNumber;
+  } else if (operation === "-") {
+    currentNumber = storedNumber - currentNumber;
+  } else if (operation === "x") {
+    currentNumber = storedNumber * currentNumber;
+  } else if (operation === "÷") {
+    currentNumber = storedNumber / currentNumber;
+  }
+  storedNumber = "";
+  operation = "";
+  output.textContent = currentNumber;
+};
+
+const addNumber = (number) => {
+  if (currentNumber.length > 10) {
+    return;
+  }
+  currentNumber += number;
+  output.textContent = currentNumber;
 };
 
 const setOperation = (newOperation) => {
-  if (operation) {
-    operate();
+  if (storedNumber === "" || operation === "") {
+    storedNumber = currentNumber;
+    currentNumber = "";
+    operation = newOperation;
+    return;
   }
+  performOperation();
   operation = newOperation;
-  storedNumber = currentNumber;
-  currentNumber = "0";
 };
 
-const operate = () => {
-  let result;
-  const storedNum = parseFloat(storedNumber);
-  const currentNum = parseFloat(currentNumber);
-  switch (operation) {
-    case "+":
-      result = storedNum + currentNum;
-      break;
-    case "-":
-      result = storedNum - currentNum;
-      break;
-    case "x":
-      result = storedNum * currentNum;
-      break;
-    case "÷":
-      result = storedNum / currentNum;
-      break;
-    default:
-      return;
+const equals = () => {
+  if (storedNumber === "" || operation === "" || currentNumber === "") {
+    return;
   }
-  currentNumber = result.toString();
-  operation = null;
-  storedNumber = "0";
+  performOperation();
 };
 
 document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("negative").addEventListener("click", negative);
+document.getElementById("percent").addEventListener("click", percent);
+document.getElementById("divide").addEventListener("click", () => setOperation("÷"));
+document.getElementById("seven").addEventListener("click", () => addNumber("7"));
+document.getElementById("eight").addEventListener("click", () => addNumber("8"));
+document.getElementById("nine").addEventListener("click", () => addNumber("9"));
+document.getElementById("multiply").addEventListener("click", () => setOperation("x"));
+document.getElementById("four").addEventListener("click", () => addNumber("4"));
+document.getElementById("five").addEventListener("click", () => addNumber("5"));
+document.getElementById("six").addEventListener("click", () => addNumber("6"));
+document.getElementById("subtract").addEventListener("click", () => setOperation("-"));
+document.getElementById("one").addEventListener("click", () => addNumber("1"));
+document.getElementById("two").addEventListener("click", () => addNumber("2"));
+document.getElementById("three").addEventListener("click", () => addNumber("3"));
+document.getElementById("add").addEventListener("click", () => setOperation("+"));
+document.getElementById("zero").addEventListener("click", () => addNumber("0"));
+document.getElementById("decimal").addEventListener("click", () => addNumber("."));
+document.getElementById("equals").addEventListener("click", equals);
 
-document.getElementById("negative").addEventListener("click", () => {
-  if (currentNumber === "0") {
-    return;
-  }
-  currentNumber = currentNumber.charAt(0) === "-" ? currentNumber.substr(1) : `-${currentNumber}`;
-  updateOutput();
-});
-
-document.getElementById("percent").addEventListener("click", () => {
-  if (currentNumber === "0") {
-    return;
-  }
-  currentNumber = (parseFloat(currentNumber) / 100).toString();
-  updateOutput();
-});
-
-document.getElementById("divide").addEventListener("click", () => {
-  setOperation("÷");
-});
-
-document.getElementById("seven").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "7" : currentNumber + "7";
-  updateOutput();
-});
-
-document.getElementById("eight").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "8" : currentNumber + "8";
-  updateOutput();
-});
-
-document.getElementById("nine").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "9" : currentNumber + "9";
-  updateOutput();
-});
-
-document.getElementById("multiply").addEventListener("click", () => {
-  setOperation("x");
-});
-
-document.getElementById("four").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "4" : currentNumber + "4";
-  updateOutput();
-});
-
-document.getElementById("five").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "5" : currentNumber + "5";
-  updateOutput();
-});
-
-document.getElementById("six").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "6" : currentNumber + "6";
-  updateOutput();
-});
-
-document.getElementById("subtract").addEventListener("click", () => {
-  setOperation("-");
-});
-
-document.getElementById("one").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "1" : currentNumber + "1";
-  updateOutput();
-});
-
-document.getElementById("two").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "2" : currentNumber + "2";
-  updateOutput();
-});
-
-document.getElementById("three").addEventListener("click", () => {
-  currentNumber = currentNumber === "0" ? "3" : currentNumber + "3";
-  updateOutput();
-});
-
-document.getElementById("add").addEventListener("click", () => {
-  setOperation("+");
-});
-
-document.getElementById("zero").addEventListener("click", () => {
-  if (currentNumber === "0") {
-    return;
-  }
-  currentNumber = currentNumber === "0" ? "0" : currentNumber + "0";
-  updateOutput();
-});
-
-document.getElementById("decimal").addEventListener("click", () => {
-  if (currentNumber.includes(".")) {
-    return;
-  }
-  currentNumber = currentNumber === "0" ? "." : currentNumber + ".";
-  updateOutput();
-});
-
-document.getElementById("equals").addEventListener("click", () => {
-  operate();
-  updateOutput();
-});
 
 
 
