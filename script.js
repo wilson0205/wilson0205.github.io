4dda715e81d3cce4290caf0c670bb731e077a1b0
wilson0205 <wilson0205@gmail.com>
2023-01-07 21:34:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-// Get a reference to all the buttons
-const buttons = document.querySelectorAll("#buttons button");
+currentNumber = "";
+previousNumber = "";
+currentOperator = "";
+updateOutput();
+}
 
-// Get a reference to the output element
-const output = document.getElementById("output");
+function handleNegative() {
+  currentNumber = (-1 * Number(currentNumber)).toString();
+  updateOutput();
+}
 
-// Create a variable to store the current number
-let currentNumber = "";
+function handlePercent() {
+  currentNumber = (Number(currentNumber) / 100).toString();
+  updateOutput();
+}
 
-// Add an event listener for each button
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    // Get the text content of the button
-    const value = button.textContent;
-
-    // Check the value of the button
-    if (value === "AC") {
-      // Clear the current number and the output
-      currentNumber = "";
-      output.textContent = "";
-    } else if (value === ".") {
-      // Add a decimal point to the current number
-      currentNumber += ".";
-      output.textContent = currentNumber;
-    } else if (value === "=") {
-      // Evaluate the current number as an expression and output the result
-      output.textContent = eval(currentNumber);
-    } else {
-      // Add the value of the button to the current number
-      currentNumber += value;
-      output.textContent = currentNumber;
-    }
-  });
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(button => {
+  button.addEventListener("click", handleNumber);
 });
 
+const operators = document.querySelectorAll(".operator");
+operators.forEach(button => {
+  button.addEventListener("click", handleOperator);
+});
 
+const equalsButton = document.getElementById("equals");
+equalsButton.addEventListener("click", handleEquals);
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", handleClear);
+
+const negativeButton = document.getElementById("negative");
+negativeButton.addEventListener("click", handleNegative);
+
+const percentButton = document.getElementById("percent");
+percentButton.addEventListener("click", handlePercent);
 
 
